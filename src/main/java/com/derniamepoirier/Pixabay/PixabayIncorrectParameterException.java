ec90c08754f2da14af282c5711c29bff2c1b657f
Alexandre DERNIAME <kiralex1995@gmail.com>
2017-11-10 17:56:02 +0100

    add additional javadoc & regenerate javadoc

--- src/main/java/com/derniamepoirier/Pixabay/PixabayIncorrectParameterException.java
+++ src/main/java/com/derniamepoirier/Pixabay/PixabayIncorrectParameterException.java
@@ -1,5 +1,8 @@
 package com.derniamepoirier.Pixabay;
 
+/**
+ * Error throwed when receiving incorrect page number or number of result per page
+ */
 public class PixabayIncorrectParameterException extends Exception {
     PixabayIncorrectParameterException(String str){
         super(str);
--- src/main/java/com/derniamepoirier/Pixabay/PixabayResponseCodeException.java
+++ src/main/java/com/derniamepoirier/Pixabay/PixabayResponseCodeException.java
@@ -1,5 +1,8 @@
 package com.derniamepoirier.Pixabay;
 
+/**
+ * Error throwed when Pixabay respond code is an error code
+ */
 public class PixabayResponseCodeException extends Exception {
     PixabayResponseCodeException(String str){
         super(str);
