cf11152f106fa6a9db9b0daf63d57e1152ddd043
Unknown <stephane.poirier@etudiant.univ-reims.fr>
2017-11-16 17:28:36 +0100

    path corrected of myPoints --> /myPoints

--- src/main/java/com/derniamepoirier/MyPointsServlet.java
+++ src/main/java/com/derniamepoirier/MyPointsServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
-@WebServlet(name = "MyPointsServlet", value="myPoints")
+@WebServlet(name = "MyPointsServlet", value="/myPoints")
 public class MyPointsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         long nbPoints = 0;
